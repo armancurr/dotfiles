511e376a1bca25b2cae1dcc329d602a048ddbb0f
Arman <contact.armankar@gmail.com>
2025-04-02 23:49:01 +0530

    vereoman

--- githubScript.ts
+++ githubScript.ts
@@ -2,7 +2,7 @@
 // @name         GitHub Geist Mono Font
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  Changes code font on GitHub to Geist Mono
+// @description  Changes code font on GitHub to Geist Mono (including code editing)
 // @author       You
 // @match        https://github.com/*
 // @match        https://gist.github.com/*
@@ -54,17 +54,49 @@
                 font-family: 'Geist Mono', monospace !important;
                 font-feature-settings: 'liga' 0 !important;
             }
+
             /* Ensure all editor views use the font */
             .react-code-view .react-code-line * {
                 font-family: 'Geist Mono', monospace !important;
             }
+
             /* GitHub's newer code view */
             .react-blob-view-header-sticky {
                 --h-code-font: 'Geist Mono', monospace !important;
             }
+
             /* GitHub's CSS variables */
             :root {
                 --mono-font: 'Geist Mono', monospace !important;
+            }
+
+            /* Code editing elements */
+            .CodeMirror,
+            .CodeMirror-lines,
+            .CodeMirror textarea,
+            .CodeMirror pre.CodeMirror-line,
+            .CodeMirror pre.CodeMirror-line-like,
+            .CodeMirror-linenumber,
+            .monaco-editor .view-lines,
+            .monaco-editor .view-line,
+            .monaco-editor .margin-view-overlays .line-numbers,
+            .monaco-editor-background,
+            .monaco-editor .inputarea,
+            .monaco-editor .editor-widget,
+            textarea.file-editor-textarea,
+            textarea.commit-create,
+            textarea.js-comment-field,
+            .commit-create-textarea,
+            .edit-file-textarea,
+            div[contenteditable="true"] {
+                font-family: 'Geist Mono', monospace !important;
+            }
+
+            /* Editor in-place edit mode */
+            .js-file-line-container [role="textbox"],
+            .js-blob-code [role="textbox"],
+            .blob-code-content [role="textbox"] {
+                font-family: 'Geist Mono', monospace !important;
             }
         `;
         document.head.appendChild(customCSS);
@@ -72,8 +104,19 @@
 
     applyGeistMono();
 
-    const observer = new MutationObserver(function() {
-        applyGeistMono();
+    const observer = new MutationObserver(function(mutations) {
+        let shouldUpdate = false;
+
+        for (const mutation of mutations) {
+            if (mutation.addedNodes.length > 0) {
+                shouldUpdate = true;
+                break;
+            }
+        }
+
+        if (shouldUpdate) {
+            applyGeistMono();
+        }
     });
 
     observer.observe(document.body, {
@@ -83,5 +126,9 @@
 
     window.addEventListener('load', function() {
         setTimeout(applyGeistMono, 1000);
+    });
+
+    document.addEventListener('pjax:end', function() {
+        setTimeout(applyGeistMono, 200);
     });
 })();
